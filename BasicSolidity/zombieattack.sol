ed2e8342571137b18bae8610065f179fcabbeb41
NguyenLoi851 <nguyensyloi851@gmail.com>
2022-03-26 18:07:56 +0700

    complete part 1

---
+++ BasicSolidity/zombieattack.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+import "./zombiehelper.sol";
+import "./safemath.sol";
+
+contract ZombieAttack is ZombieHelper {
+    using SafeMath for uint;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
+
+    uint256 randNonce = 0;
+
+    uint256 attackVictoryProbability = 70;
+
+    function randMod(uint256 _modulus) internal returns (uint256) {
+        randNonce=randNonce.add(1);
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.timestamp, msg.sender, randNonce)
+                )
+            ) % _modulus;
+    }
+
+    function attack(uint256 _zombieId, uint256 _targetId)
+        external
+        onlyOwnerOf(_zombieId)
+    {
+        Zombie storage myZombie = zombies[_zombieId];
+        Zombie storage enemyZombie = zombies[_targetId];
+        uint256 rand = randMod(100);
+        if (rand <= attackVictoryProbability) {
+            myZombie.winCount=myZombie.winCount.add(1);
+            myZombie.level=myZombie.level.add(1);
+            enemyZombie.lossCount=enemyZombie.lossCount.add(1);
+            feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+        } else {
+            myZombie.lossCount=myZombie.lossCount.add(1);
+            enemyZombie.winCount=enemyZombie.winCount.add(1);
+            _triggerCooldown(myZombie);
+        }
+    }
+}
---
+++ BasicSolidity/zombiehelper.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.13;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+    uint256 levelUpFee = 0.001 ether;
+
+    modifier aboveLevel(uint256 _level, uint256 _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+
+    function withdraw() external onlyOwner {
+        address payable _owner = payable(address(owner()));
+        _owner.transfer(address(this).balance);
+    }
+
+    function setLevelUpFee(uint256 _fee) external onlyOwner {
+        levelUpFee = _fee;
+    }
+
+    function levelUp(uint256 _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
+    }
+
+    function changeName(uint256 _zombieId, string calldata _newName)
+        external
+        aboveLevel(2, _zombieId)
+        onlyOwnerOf(_zombieId)
+    {
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeDna(uint256 _zombieId, uint256 _newDna)
+        external
+        aboveLevel(20, _zombieId)
+        onlyOwnerOf(_zombieId)
+    {
+        zombies[_zombieId].dna = _newDna;
+    }
+
+    function getZombiesByOwner(address _owner)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        uint256[] memory result = new uint256[](ownerZombieCount[_owner]);
+        uint256 counter = 0;
+        for (uint256 i = 0; i < zombies.length; i++) {
+            if (zombieToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
+}
