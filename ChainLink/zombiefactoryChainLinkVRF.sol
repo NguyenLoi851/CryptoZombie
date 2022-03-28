16d4990d84e02803903d7b0cd0e6308a46b6106a
NguyenLoi851 <nguyensyloi851@gmail.com>
2022-03-28 12:49:49 +0700

    update ChainLink code

--- ChainLink/zombiefactoryChainLinkVRF.sol
+++ ChainLink/zombiefactoryChainLinkVRF.sol
@@ -3,9 +3,13 @@ pragma solidity ^0.8.13;
 
 import "./node_modules/@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract ZombieFactory {
+contract ZombieFactory is VRFConsumerbase {
     uint256 dnaDigits = 16;
     uint256 dnaModulus = 10**dnaDigits;
+
+    bytes32 public keyHash;
+    uint256 public fee;
+    uint256 public randomResult;
 
     struct Zombie {
         string name;
@@ -13,6 +17,17 @@ contract ZombieFactory {
     }
 
     Zombie[] public zombies;
+
+    constructor()
+        public
+        VRFConsumerBase(
+            0xb3dCcb4Cf7a26f6cf6B120Cf5A73875B7BBc655B, // VRF Coordinator
+            0x01BE23585060835E02B77ef475b0Cc51aA1e0709 // LINK Token
+        )
+    {
+        keyHash = 0x2ed0feb3e7fd2022120aa84fab1945545a9f2ffc9076fd6156fa96eaff4c1311;
+        fee = 100000000000000000;
+    }
 
     function _createZombie(string memory _name, uint256 _dna) private {
         zombies.push(Zombie(_name, _dna));
