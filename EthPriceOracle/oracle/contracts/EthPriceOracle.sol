28c733cb2a95faf6d28ce5073ea3fe6a505c3e6a
NguyenLoi851 <nguyensyloi851@gmail.com>
2022-04-01 19:56:29 +0700

    fix to use roles

--- EthPriceOracle/oracle/contracts/EthPriceOracle.sol
+++ EthPriceOracle/oracle/contracts/EthPriceOracle.sol
@@ -6,8 +6,8 @@ import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 import "./CallerContractInterface.sol";
 
 contract EthPriceOracle is AccessControl{
-  RoleData private owners;
-  RoleData private oracles;
+  bytes32 public constant OWNERS = keccak256("OWNERS");
+  bytes32 public constant ORACLES = keccak256("ORACLES");
   using SafeMath for uint256;
   uint private randNonce = 0;
   uint private modulus = 1000;
@@ -19,47 +19,49 @@ contract EthPriceOracle is AccessControl{
     address callerAddress;
     uint256 ethPrice;
   }
-  mapping (uint256=>Response) requestIdToResponse;
+  mapping (uint256=>Response[]) requestIdToResponse;
   event GetLatestEthPriceEvent(address callerAddress, uint id);
   event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
   event AddOracleEvent(address oracleAddress);
   event RemoveOracleEvent(address oracleAddress);
   event SetThresholdEvent (uint threshold);
-  constructor (address _owner) public {
-    grantRole(owners,_owner);
+
+  constructor (address _owner){
+    grantRole(OWNERS, _owner);
   }
+
   function addOracle (address _oracle) public {
-    require(owners.hasRole(msg.sender), "Not an owner!");
-    require(!oracles.hasRole(_oracle), "Already an oracle!");
-    oracles.grantRole(_oracle);
+    require(hasRole(OWNERS,msg.sender), "Not an owner!");
+    require(!hasRole(ORACLES,_oracle), "Already an oracle!");
+    grantRole(ORACLES,_oracle);
     numOracles++;
     emit AddOracleEvent(_oracle);
   }
   function removeOracle (address _oracle) public {
-    require(owners.hasRole(msg.sender), "Not an owner!");
-    require(oracles.hasRole(_oracle), "Not an oracle!");
+    require(hasRole(OWNERS,msg.sender), "Not an owner!");
+    require(hasRole(ORACLES,_oracle), "Already an oracle!");
     require (numOracles > 1, "Do not remove the last oracle!");
-    oracles.revokeRole(_oracle);
+    revokeRole(ORACLES,_oracle);
     numOracles--;
     emit RemoveOracleEvent(_oracle);
   }
   function setThreshold (uint _threshold) public {
-    require(owners.hasRole(msg.sender), "Not an owner!");
+    require(hasRole(OWNERS,msg.sender), "Not an owner!");
     THRESHOLD = _threshold;
     emit SetThresholdEvent(THRESHOLD);
   }
   function getLatestEthPrice() public returns (uint256) {
     randNonce++;
-    uint id = uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % modulus;
+    uint id = uint(keccak256(abi.encodePacked(block.timestamp, msg.sender, randNonce))) % modulus;
     pendingRequests[id] = true;
     emit GetLatestEthPriceEvent(msg.sender, id);
     return id;
   }
   function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public {
-    require(oracles.has(msg.sender), "Not an oracle!");
+    require(hasRole(ORACLES,msg.sender), "Not an oracle!");
     require(pendingRequests[_id], "This request is not in my pending list.");
     Response memory resp;
-    resp = Response(msg.sender, _callerAddress, _ethPrice);
+    resp = Response(payable(msg.sender), payable(_callerAddress), _ethPrice);
     requestIdToResponse[_id].push(resp);
     uint numResponses = requestIdToResponse[_id].length;
     if (numResponses == THRESHOLD) {
